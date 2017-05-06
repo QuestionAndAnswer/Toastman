971cd4ff7fd897e032dee9064f765aeea8e820d7
Artsiom Miksiuk <artsiom.miksiuk@LeverX.local>
2017-05-07 00:59:16 +0300

    Cli added. Rewrited

--- index.js
+++ index.js
@@ -1,92 +1,106 @@
-const newman = require("newman");
 const _ = require("underscore");
 const extend = require("extend");
+
 /**
- * Toastman class that provides basic functionality
- * @class
+ * Flatten collection items with renaming of the requests.
+ * Requests renamed relatively subfolders where they layed.
+ * @param {object} rawCollection Postman raw collection object
+ * @return {object} Map of the requests by their's names
  */
-class Toastman {
-    constructor (path) {
-        this._collection = require(path);
-        this._chains = [];
-    }
+function normalizeColletionItems (rawCollection) {
+    let foldersMap = _.indexBy(rawCollection.item, "name");
+    let toReturnMap = {};
 
-    /**
-     * @type Toastman~chain
-     * @property {string} name Chain's name
-     * @property {string[]} requests Requests name array to execute in this chain
-     */
+    for(let folderName in foldersMap) {
+        let folder = foldersMap[folderName];
 
-    /**
-     * Add ne wchain into run
-     * @param {Toastman~chain} chain New chain object
-     */
-    addChain(chain) {
-        this._chains.push(chain);
-        return this;
-    }
-
-    /**
-     * Newman's run method overriding
-     * Same parameters as in original.
-     * @return {EventEmiter} Newman's EventEmiter instance
-     */
-    run(options) {
-        let collectionToRun = this.getFakeCollectionToRun();
-        options.collection = collectionToRun;
-
-        return newman.run.apply(newman, arguments);
-    }
-
-    /**
-     * Returns new fake collection according to added chains.
-     * @return {Collection} New collection with all added chains on the call time 
-     */
-    getFakeCollectionToRun () {
-        let requestsMap = this._getNormalizedCollectionItems();
-        
-        let collectionToRun = extend(true, {}, this._collection);
-        collectionToRun.item = [];
-        
-        this._chains.forEach((chain, index) => {
-            let folder = {
-                name: chain.name || index,
-                item: []
-            };
-
-            collectionToRun.item.push(folder);
-
-            chain.requests.forEach(reqName => {
-                let reqInstance = requestsMap[reqName];
-                folder.item.push(reqInstance);
+        let requestsMap = _.indexBy(folder.item, "name");
+        Object.keys(requestsMap)
+            .forEach(key => {
+                let newName = folderName + "/" + key;
+                toReturnMap[newName] = requestsMap[key];
+                toReturnMap[newName].name = newName;
             });
-        });
-
-        return collectionToRun;
     }
 
-    /**
-     * Flatten collection items with renaming of the requests.
-     * Requests renamed relatively subfolders where they layed.
-     */
-    _getNormalizedCollectionItems () {
-        let foldersMap = _.indexBy(this._collection.item, "name");
-        let toReturnMap = {};
-
-        for(let folderName in foldersMap) {
-            let folder = foldersMap[folderName];
-
-            let requestsMap = _.indexBy(folder.item, "name");
-            Object.keys(requestsMap)
-                .forEach(key => {
-                    let newName = folderName + "/" + key;
-                    toReturnMap[newName] = requestsMap[key];
-                    toReturnMap[newName].name = newName;
-                });
-        }
-
-        return toReturnMap;
-    }
+    return toReturnMap;
 }
 
-module.exports = Toastman;
+/**
+ * Returns new fake collection according to added chains.
+ * @param {object} requestsMap Requests map from @see normalizeCollectionItems
+ * @param {object} rawCollection Raw postman collection object
+ * @param {object[]} chains Array of chains declarations
+ * @return {object} New collection with all added chains on the call time 
+ */
+function _buildChainsCollection (requestsMap, rawCollection, chains) {
+    let collectionToRun = extend(true, {}, rawCollection);
+    collectionToRun.item = [];
+    
+    chains.forEach((chain, index) => {
+        let folder = {
+            name: chain.name || index,
+            item: []
+        };
+
+        collectionToRun.item.push(folder);
+
+        chain.requests.forEach(reqName => {
+            let reqInstance = requestsMap[reqName];
+            folder.item.push(reqInstance);
+        });
+    });
+
+    return collectionToRun;
+}
+
+/**
+ * Constructs new collection using chains
+ * @param {object|string} collection Postman collection object or path to collection file
+ * @param {object|string} chains Toastman chains object or path to chains file
+ * @return {object} Transformed postman collection
+ */
+function buildChainsCollection (collection, chains) {
+    let collectionObj = typeof collection === "string" ? require(collection) : collection;
+    let chainsObj = typeof chains === "string" ? require(chains) : chains;
+
+    if(!collectionObj) {
+        throw new Error("Postman collection object is empty. Check file path or file content");
+    }
+
+    if(!chainsObj) {
+        throw new Error("Toastman chains object is empty. Check file path or file content");
+    }
+
+    let requestsMap = normalizeColletionItems(collectionObj);
+    return _buildChainsCollection(requestsMap, collectionObj, chainsObj.chains);
+}
+
+
+if(require.main === module) {
+    const ArgumentParser = require("argparse").ArgumentParser;
+    const fs = require("fs");
+    const path = require("path");
+
+    const parser = new ArgumentParser({
+        version: "0.1.0",
+        addHelp: true,
+        description: "Transform collection into chains"
+    });
+
+    parser.addArgument("--postman-collection-path", { help: "Path to collection file", required: true });
+    parser.addArgument("--toastman-chains-path", { help: "Path to toastman file", required: true });
+    parser.addArgument("--out-collection-path", { help: "Path to output collection file", defaultValue: ".\\toastman_collection.json"});
+
+    var args = parser.parseArgs();
+
+    let outCollection = buildChainsCollection(path.resolve(args.postman_collection_path), path.resolve(args.toastman_chains_path));
+
+    let outPath = path.resolve(args.out_collection_path);
+    
+    fs.writeFileSync(outPath, JSON.stringify(outCollection, null, 4), "utf-8");
+
+    process.stdout.write(outPath);
+} else {
+    module.exports = buildChainsCollection;
+}
--- test/index.js
+++ test/index.js
@@ -2,42 +2,72 @@ const mocha = require("mocha");
 const assert = require("chai").assert;
 
 const http = require("http");
-const Toastman = require("./../index.js");
+const toastman = require("./../index.js");
+const newman = require("newman");
+const path = require("path");
+const shell = require("shelljs");
 
-const pathToCollection = "./test/test_collection.json";
+const postmanCollectionFileName = "test_collection.json";
+const toastmanChainsFileName = "test_chains.json";
+const pathToPostmanCollection = path.resolve("test", postmanCollectionFileName);
+const pathToToastmanChains = path.resolve("test", toastmanChainsFileName);
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200).end();
+    res.writeHead(200);
+
+    res.end("OK");
 }); 
 
 before(() => {
     server.listen(8000);
 });
 
-describe("Global", () => {
-    it("testRun", () => {
-        let toastman = new Toastman(pathToCollection);
+describe("Import run", () => {
+    function testRun(outCollection, done) {
+        assert.strictEqual(outCollection.item.length, 2, "Two chains");
+        assert.strictEqual(outCollection.item[0].item.length, 3, "Tree items in chain1");
+        assert.strictEqual(outCollection.item[1].item.length, 1, "One item in chain2");
 
-        toastman
-            .addChain({
-                name: "chain1",
-                requests: ["1/1", "2/2", "2/2"]
-            }).addChain({
-                name: "chain2",
-                requests: ["1/1"]
+        newman.run({
+            collection: outCollection,
+            reporter: "cli"
+        })
+            .on("done", () => {
+                assert.isOk(true, "Not thrown an exception");
+                done();
             });
+    }
 
-        let collectionToRun = toastman.getFakeCollectionToRun();
+    it("passing files pathes", (done) => {
+        let outCollection = toastman(pathToPostmanCollection, pathToToastmanChains);
 
-        assert.strictEqual(collectionToRun.item.length, 2, "Two chains");
-        assert.strictEqual(collectionToRun.item[0].item.length, 3, "Tree items in chain1");
-        assert.strictEqual(collectionToRun.item[1].item.length, 1, "One item in chain2");
+        testRun(outCollection, done);
+    });
 
-        toastman.run({
-            reporters: "cli"
-        });
 
-        assert.isOk(true, "Not thrown an exception");
+    it("passing objects", (done) => {
+        let postmanCollection = require(pathToPostmanCollection);
+        let toastmanChains = require(pathToToastmanChains);
+        
+        let outCollection = toastman(postmanCollection, toastmanChains);
+
+        testRun(outCollection, done);
+    });
+});
+
+describe("Run via CLI", () => {
+    it("returns file path", () => {
+        let cmd = "node .\\index.js ";
+        cmd += "--postman-collection-path .\\test\\" + postmanCollectionFileName + " ";
+        cmd += "--toastman-chains-path .\\test\\" + toastmanChainsFileName + " ";
+        cmd += "--out-collection-path .\\test\\custom_toastman_collection.json  ";
+
+        let outPath = shell.exec(cmd, { silent: true }).toString();
+
+        assert.isOk(outPath.indexOf("\\test\\custom_toastman_collection.json") !== -1, "Return path is wrong");
+
+        let collection = require(outPath);
+        assert.isOk(collection, "Collecton not readable");
     });
 });
 
